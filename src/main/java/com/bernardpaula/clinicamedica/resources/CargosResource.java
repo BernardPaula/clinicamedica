7ad1d16da44d78ca32b8a7cbddf3bdcd45e84e3d
Bernard <bernardstudioart@gmail.com>
2022-06-07 15:42:25 -0300

    findPage teste

--- src/main/java/com/bernardpaula/clinicamedica/resources/CargosResource.java
+++ src/main/java/com/bernardpaula/clinicamedica/resources/CargosResource.java
@@ -1,18 +1,18 @@
 package com.bernardpaula.clinicamedica.resources;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bernardpaula.clinicamedica.domain.Cargos;
 import com.bernardpaula.clinicamedica.services.CargosService;
@@ -68,6 +68,15 @@ public class CargosResource {
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public ResponseEntity<List<Cargos>> findAll(){
 		List<Cargos> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	public ResponseEntity<Page<Cargos>> findPage(
+			@RequestParam(value = "page", defaultValue = "0")Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nm_cargo")String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction){
+		Page<Cargos> list = service.findPage(page, linesPerPage, orderBy, direction);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/bernardpaula/clinicamedica/services/CargosService.java
+++ src/main/java/com/bernardpaula/clinicamedica/services/CargosService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bernardpaula.clinicamedica.domain.Cargos;
@@ -57,4 +60,8 @@ public class CargosService {
 		return repo.findAll();
 	}
 	
+	public Page<Cargos> findPage(Integer nome, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(nome, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
 }
